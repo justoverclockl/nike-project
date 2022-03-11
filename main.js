be8ce894d194ec249c0c6bf386a1b776baaa7b38
Marco Colia <info@justoverclock.it>
2022-03-11 16:34:51 +0100

    minor changes

--- main.js
+++ main.js
@@ -10,8 +10,8 @@ let swiper = new Swiper('.swiper', {
 
 // slider delle cards
 const next = document.getElementById('next');
+const div = document.querySelector('.cards-container');
 next.addEventListener('click', () => {
-    const div = document.querySelector('.cards-container');
     div.scrollBy({
         left: 600,
         behavior: 'smooth'
@@ -20,8 +20,7 @@ next.addEventListener('click', () => {
 
 const prev = document.getElementById('previous');
 prev.addEventListener('click', () => {
-    const diva = document.querySelector('.cards-container');
-    diva.scrollBy({
+    div.scrollBy({
         left: -600,
         behavior: 'smooth'
     });
@@ -39,12 +38,12 @@ closePopup.addEventListener('click', () => {
 })
 
 // classe per il modulo login
-let newUser = [];
 
 class Login {
-    constructor(email, password) {
+    constructor(email, password, userId, registrationDate) {
         this.email = email
         this.password = password
+        this.registrationDate = new Date().toISOString()
     }
 
     toJson() {
@@ -55,25 +54,50 @@ class Login {
         return `Grazie per esserti registrato. La tua email è ${this.email}`
     }
 
+    displayLoginEmail() {
+        return this.email
+    }
+
     static fromJson(json) {
         return JSON.parse(json)
     }
+
+}
+
+function addUser(email,password,registrationDate){
+    const user = new Login(email,password,registrationDate);
+    let newUser = [];
+    newUser.push([email,password,registrationDate]);
+    console.log(newUser)
+    user.displayLoginMessage()
 }
 
 
-function addUser(e) {
+
+/*function addUser(e) {
     e.preventDefault()
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
     const popup = document.getElementById('popup-text');
-    const loginForm = document.getElementById('loginform')
+    const loginForm = document.getElementById('loginform');
+    const navLinks = document.getElementById('nav-links');
     const userNew = new Login(email, password)
     newUser.push(userNew)
     console.log(newUser)
     loginForm.setAttribute('style', 'display:none !important');
     popup.innerHTML = userNew.displayLoginMessage()
-}
-
+    localStorage.setItem("user", userNew.toJson());
+    if ("user" in localStorage) {
+        navLinks.innerHTML = `<ul>
+                    <li class="links">Bentornato ${userNew.displayLoginEmail()}</li>
+                </ul>`
+    }
+}*/
 
 const submitButton = document.getElementById('login');
-submitButton.addEventListener('click', addUser)
+submitButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    const emailValue = document.getElementById('email').value;
+    const passwordValue = document.getElementById('password').value;
+    addUser(emailValue, passwordValue, new Date().toISOString())
+})
