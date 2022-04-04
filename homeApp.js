9b00db81e4c03cc19d68b6a5b289f2e36402f907
simone d'ambrosio <simonedamb@hotmail>
2022-04-04 16:38:30 +0200

    nike-project-35

--- homeApp.js
+++ homeApp.js
@@ -1,4 +1,10 @@
 const logIN = document.getElementById("accedi");
 logIN.addEventListener("click", () => {
-  const popUp = document.getElementById("pop-up").style.display = "block";
+  const popUp = (document.getElementById("pop-up-container").style.display =
+    "block");
+});
+const logOut = document.getElementById("close");
+logOut.addEventListener("click", () => {
+  const pop = (document.getElementById("pop-up-container").style.display =
+    "none");
 });
