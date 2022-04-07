50b3f38fa2eebf2962a4a1db92a76f132da38af2
simone d'ambrosio <simonedamb@hotmail>
2022-04-07 16:30:21 +0200

    nike-project-feature-js

--- homeApp.js
+++ homeApp.js
@@ -9,4 +9,23 @@ logOut.addEventListener("click", () => {
     "none");
 });
 
+const closeCookie = document.getElementById("accept");
+setTimeout(() => {
+  document.getElementById("pop-up-cookie").style.display = "block";
+}, 5000),
+  closeCookie.addEventListener("click", () => {
+    document.getElementById("pop-up-cookie").style.display = "none";
+  });
 
+const buttonNext = document.getElementById("next");
+const list = document.getElementsByClassName("carousel-list");
+
+function scroll() {
+  buttonNext.addEventListener("click", () => {
+    list.scoop({
+      left: -700,
+      behavior: "smooth",
+    });
+  });
+  const prev = document.getElementById;
+}
