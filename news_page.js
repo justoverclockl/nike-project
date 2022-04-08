a389e71f9cfc4a5a538b017fd84f0fa800281a27
Amelia Tedeschi <tedeschiamelia@gmail.com>
2022-04-08 15:47:24 +0200

    news-page v2

--- news_page.js
+++ news_page.js
@@ -1,7 +1,32 @@
-function openInput() {
-  document.getElementById("search").style.display = "block";
-}
+let iconSearch = document.getElementById("icon-search");
+iconSearch.addEventListener("mouseover", () => {
+  const search = (document.getElementById("input-search").style.display =
+    "block");
+});
 
-function closeInput() {
-  document.getElementById("search").style.display = "none";
-}
+iconSearch.addEventListener("mouseout", () => {
+  const closeSearch = (document.getElementById("input-search").style.display =
+    "none");
+});
+
+let iconMenu = document.getElementById("icon-menu");
+iconMenu.addEventListener("click", () => {
+  const menu = (document.getElementById("menu").style.display = "block");
+});
+
+/*let lista = document.querySelectorAll("li");
+for (let i = 0; i < lista.length; i++) {
+
+  lista[i].addEventListener("mouseover", function () {
+    
+    
+    this.style.color = "#818181";
+    this.style.transition = "0.3";
+
+  });
+}*/
+
+const closeIconMenu = document.getElementById("closebtn");
+closeIconMenu.addEventListener("click", () => {
+  const closeMenu = (document.getElementById("menu").style.display = "none");
+});
