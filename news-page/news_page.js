f52b469812b4b688c78a564ae00bc5e912b5e002
Amelia Tedeschi <tedeschiamelia@gmail.com>
2022-04-13 10:05:28 +0200

    news-page v8

--- news-page/news_page.js
+++ news-page/news_page.js
@@ -1,4 +1,4 @@
-// mostra search
+// exhibition search
 let search = document.getElementById("icon-search");
 search.addEventListener("click", () => {
   const pageSearch = (document.getElementById("page-search").style.display =
@@ -12,19 +12,7 @@ closeSearch.addEventListener("click", () => {
   ).style.display = "none");
 });
 
-/* mostra input
-let iconSearch = document.getElementById("icon-search");
-iconSearch.addEventListener("mouseover", () => {
-  const search = (document.getElementById("input-search").style.display =
-    "block");
-});
-
-iconSearch.addEventListener("mouseout", () => {
-  const closeSearch = (document.getElementById("input-search").style.display =
-    "none");
-});*/
-
-// mostra menu
+// exhibition menu
 let iconMenu = document.getElementById("icon-menu");
 iconMenu.addEventListener("click", () => {
   let menu = (document.getElementById("menu").style.display = "block");
@@ -34,26 +22,3 @@ const closeIconMenu = document.getElementById("closebtn");
 closeIconMenu.addEventListener("click", () => {
   const closeMenu = (document.getElementById("menu").style.display = "none");
 });
-
-const list = document.getElementById("list");
-
-function enterList(event) {
-  event.currentTarget.style.color = "gray";
-}
-
-function exitList(event) {
-  event.currentTarget.style.color = "white";
-}
-
-function hoverElement(event) {
-  event.target.style.color = "white";
-}
-
-function hoveroutElement(event) {
-  event.target.style.color = "inherit";
-}
-
-list.addEventListener("mouseenter", enterList);
-list.addEventListener("mouseleave", exitList);
-list.addEventListener("mouseover", hoverElement);
-list.addEventListener("mouseout", hoveroutElement);
